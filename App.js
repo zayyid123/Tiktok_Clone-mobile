fdeb299834acd593e8785bef3d4c31c3e4051936
zayyid123 <mochamadzayyid@gmail.com>
2023-10-23 23:24:47 +0700

    add heroicons

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { SparklesIcon } from "react-native-heroicons/solid";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>zay</Text>
       <StatusBar style="auto" />
+      <SparklesIcon size={30} fill={'#000'}/>
+      <Text>zay</Text>
     </View>
   );
 }
