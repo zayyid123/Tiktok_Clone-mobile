8c321640ec41affae8815998ffe51dfffa610cc0
zayyid123 <mochamadzayyid@gmail.com>
2023-10-25 15:07:32 +0700

    fix feed

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, StyleSheet, Text, View, PixelRatio } from 'react-native';
+import { Dimensions, StyleSheet, Text, View, PixelRatio, Image, TouchableOpacity } from 'react-native';
 import { SparklesIcon } from "react-native-heroicons/solid";
 import VerticalViewPager from "react-native-vertical-view-pager";
 import NavigationBottom from './components/NavigationBottom';
@@ -7,26 +7,15 @@ import NavbarTop from './components/NavbarTop';
 import { Video } from 'expo-av';
 import { useEffect, useRef, useState } from 'react';
 import { FlatList } from 'react-native';
+import TextTicker from 'react-native-text-ticker';
+
+// icon
+import { MusicalNoteIcon, PlusIcon } from "react-native-heroicons/outline";
 
 const pixelRatio = PixelRatio.get();
 
 let { width, height } = Dimensions.get("window");
 height = height - (10 * pixelRatio)
-
-const feed =[
-  {
-    id: 0,
-    video: require('./assets/feed/status1.mp4'),
-  },
-  {
-    id: 1,
-    video: require('./assets/feed/status2.mp4'),
-  },
-  {
-    id: 2,
-    video: require('./assets/feed/status3.mp4'),
-  }
-]
 
 const play = async (ref) => {
   if (ref == null) {
@@ -63,14 +52,58 @@ const stop = async (ref) => {
 }
 
 export default function App() {
-  const [status, setstatus] = useState()
+  const [feed, setfeed] = useState([
+    {
+      id: 0,
+      video: require('./assets/feed/status1.mp4'),
+      desc: 'Gus Bahaudin Nursalim',
+      music: 'Gus Baha - By Mochamad Muzayyid Al Hakim',
+      isLiked: true,
+      isSaved: true
+    },
+    {
+      id: 1,
+      video: require('./assets/feed/status2.mp4'),
+      desc: 'Anak Kecil Palestina sedang menangisi ibunya',
+      music: 'Original Sound - By Mochamad Muzayyid Al Hakim',
+      isLiked: false,
+      isSaved: false
+    },
+    {
+      id: 2,
+      video: require('./assets/feed/status3.mp4'),
+      desc: '#SAVEPALESTINA',
+      music: 'Memory Boot - By Mochamad Muzayyid Al Hakim',
+      isLiked: false,
+      isSaved: false
+    }
+  ])
   const mediaRefs = []
+
+  const handleLike = (id) => {
+    const updatedFeed = [...feed];
+
+    const itemIndex = updatedFeed.findIndex(item => item.id === id);
+
+    updatedFeed[itemIndex].isLiked = !updatedFeed[itemIndex].isLiked;
+  
+    setfeed(updatedFeed);
+  }
+
+  const handleSave = (id) => {
+    const updatedFeed = [...feed];
+
+    const itemIndex = updatedFeed.findIndex(item => item.id === id);
+
+    updatedFeed[itemIndex].isSaved = !updatedFeed[itemIndex].isSaved;
+  
+    setfeed(updatedFeed);
+  }
 
   const onViewableItemsChanged = useRef(({ changed }) => {
     changed.forEach(element => {
       const cell = mediaRefs[element.key]
       if (cell) {
-        console.log(element)
         if (element.isViewable) {
           play(cell)
         } else {
@@ -105,7 +138,7 @@ export default function App() {
             viewabilityConfig={{
               itemVisiblePercentThreshold: 1
             }}
-            renderItem={({item}) => {
+            renderItem={({item, index}) => {
               return(
                 <View 
                   style={{
@@ -113,7 +146,7 @@ export default function App() {
                     width,
                     height,
                   }}
-                  className='flex-1 justify-center w-full items-center relative bottom-8'
+                  className='flex-1 justify-center w-full items-center relative bottom-8 relative'
                 >
                   <View 
                     style={{
@@ -138,6 +171,195 @@ export default function App() {
                       }}
                       useNativeControls={false}
                     />
+                  </View>
+
+                  {/* information */}
+                  <View
+                    className='absolute bottom-0 right-0 left-0 z-40 mb-3 mx-2'
+                  >
+                    <View
+                      className='flex-row items-end justify-between'
+                    >
+                      {/* kiri */}
+                      <View>
+                        {/* name */}
+                        <View className='mb-3'>
+                          <Text className='text-white text-xl font-bold'>zay</Text>
+                        </View>
+
+                        {/* description */}
+                        <View className='mb-3'>
+                          <Text className='text-white text-lg w-[250px]'>{item.desc}</Text>
+                        </View>
+
+                        {/* name song */}
+                        <View
+                          className='flex-row items-center gap-2 w-[250px]'
+                        >
+                          <MusicalNoteIcon size={20} color={'#fff'}/>
+                          <TextTicker
+                            className='text-white text-sm'
+                            duration={4000}
+                            loop
+                            bounce={false}
+                            repeatSpacer={70}
+                            marqueeDelay={1000}
+                            shouldAnimateTreshold={40}
+                          >
+                            {item.music}
+                          </TextTicker>
+                        </View>
+                      </View>
+
+                      {/* kanan */}
+                      <View
+                        className='items-center'
+                      >
+                        {/* icon profile */}
+                        <View
+                          className='items-center mb-3'
+                        >
+                          <View
+                            className='bg-white p-[2px] rounded-full items-center justify-center'
+                          >
+                            <Image 
+                              source={{uri: 'https://lh3.googleusercontent.com/ogw/AKPQZvwdDLyYvI05P8R39uRv2Q5P2fJuKSREVocASETn1FU'}}
+                              style={{
+                                width: 35,
+                                height: 35,
+                                resizeMode: "contain",
+                              }}
+                              className='rounded-full'
+                            />
+                          </View>
+
+                          <View
+                            className='bg-[#E14763] p-[2px] rounded-full mt-[-8px]'
+                          >
+                            <PlusIcon size={16} color={'#fff'}/>
+                          </View>
+                        </View>
+
+                        {/* icon love */}
+                        <View
+                          className='items-center mb-3'
+                        >
+                          <TouchableOpacity
+                            onPress={() => {
+                              handleLike(index)
+                            }}
+                          >
+                            {
+                              item.isLiked ?
+                              <Image 
+                                source={require('./assets/icon/loved.png')}
+                                style={{
+                                  width: 30,
+                                  height: 30,
+                                  resizeMode: "contain"
+                                }}
+                                className='mb-2'
+                              />
+                              :
+                              <Image 
+                                source={require('./assets/icon/love.png')}
+                                style={{
+                                  width: 30,
+                                  height: 30,
+                                  resizeMode: "contain"
+                                }}
+                                className='mb-2'
+                              />
+                            }
+                          </TouchableOpacity>
+                          <Text className='text-white font-bold'>20.2K</Text>
+                        </View>
+
+                        {/* icon comment */}
+                        <View
+                          className='items-center mb-3'
+                        >
+                          <TouchableOpacity>
+                            <Image 
+                              source={require('./assets/icon/comment.png')}
+                              style={{
+                                width: 30,
+                                height: 30,
+                                resizeMode: "contain"
+                              }}
+                              className='mb-2'
+                            />
+                          </TouchableOpacity>
+                          <Text className='text-white font-bold'>360</Text>
+                        </View>
+
+                        {/* icon save */}
+                        <View
+                          className='items-center mb-3'
+                        >
+                          <TouchableOpacity
+                            onPress={() => {
+                              handleSave(index)
+                            }}
+                          >
+                            {
+                              item.isSaved ?
+                              <Image 
+                                source={require('./assets/icon/ribbon_saved.png')}
+                                style={{
+                                  width: 30,
+                                  height: 30,
+                                  resizeMode: "contain"
+                                }}
+                                className='mb-2'
+                              />
+                              :
+                              <Image 
+                                source={require('./assets/icon/ribbon.png')}
+                                style={{
+                                  width: 30,
+                                  height: 30,
+                                  resizeMode: "contain"
+                                }}
+                                className='mb-2'
+                              />
+                            }
+                          </TouchableOpacity>
+                          <Text className='text-white font-bold'>360</Text>
+                        </View>
+
+                        {/* icon share */}
+                        <View
+                          className='items-center mb-6'
+                        >
+                          <TouchableOpacity>
+                            <Image 
+                              source={require('./assets/icon/share.png')}
+                              style={{
+                                width: 30,
+                                height: 30,
+                                resizeMode: "contain"
+                              }}
+                              className='mb-2'
+                            />
+                          </TouchableOpacity>
+                          <Text className='text-white font-bold'>300</Text>
+                        </View>
+
+                        {/* image sound */}
+                        <View>
+                          <Image
+                            source={{uri: 'https://lh3.googleusercontent.com/ogw/AKPQZvwdDLyYvI05P8R39uRv2Q5P2fJuKSREVocASETn1FU'}}
+                            style={{
+                              width: 30,
+                              height: 30,
+                              resizeMode: "contain"
+                            }}
+                            className='rounded-full'
+                          />
+                        </View>
+                      </View>
+                    </View>
                   </View>
                 </View>
               )
